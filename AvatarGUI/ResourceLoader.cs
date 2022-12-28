fae604a5f26003d9a958069b07c17551e033067f
Germán Salinas <germanesalinas@gmail.com>
2022-12-27 21:34:44 -0300

    .

---
+++ AvatarGUI/ResourceLoader.cs
@@ -0,0 +1,85 @@
+﻿using AvatarGUI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AvatarGUI
+{
+    sealed class ResourceLoader
+    {
+        private static readonly ResourceLoader instance 
+            = new ResourceLoader(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\Recursos.json");
+
+        private static readonly string filepath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+        private Resources resources;
+
+        private ResourceLoader(string filePath)
+        {
+            resources = JsonConvert.DeserializeObject<Resources>(File.ReadAllText(filePath));
+        }
+
+        private List<string> GetBackgroundImages()
+        {
+            try {
+                DirectoryInfo d = new DirectoryInfo(filepath + @"\Imagenes");
+                Regex reg = new Regex(@"\w*.(png|jpg)");
+                FileInfo[] Files = d.GetFiles();
+                List<string> images = new List<string>();
+                foreach (FileInfo file in Files)
+                {
+                    if (reg.IsMatch(file.Name))
+                        images.Add(file.Name);
+                }
+                return images;
+            }catch (Exception e)
+            {
+                return new List<string>();
+            }
+        }
+
+        public static ResourceLoader Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public List<string> GetPrefabs()
+        {
+            return resources.characters;
+        }
+
+        public List<string> GetAudios(string folderName)
+        {
+            List<string> allAudios = new List<string>();
+            folderName = folderName == null ? "" : folderName;
+
+            foreach (string d in Directory.GetDirectories(filepath + @"\Audio"))
+            {
+                if (d.Substring(d.LastIndexOf("\\")+1).Contains(folderName) || folderName == "")
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        if (f.EndsWith(".wav"))
+                            allAudios.Add(d.Substring(d.LastIndexOf("\\")+1) + @"/" + f.Substring(f.LastIndexOf("\\")+1));
+                    }
+                }
+            }
+
+            return allAudios;
+        }
+
+        public List<string> GetBackgrounds()
+        {
+            return resources.backgrounds.Concat(GetBackgroundImages()).ToList();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Thesis-Mind-Theory (Client)
+Grade thesis for my systems engineering degree, consisting on a Unity player intended on playing real-life-like routines through a series of human like avatars and environments.
+The user can dictate which models to use, where to position them, and a step by step action taken by one of the avatars to display some scene which can denote a situation for the psychologist's
+patient to observe and empathize to, through animations and audio clips or subtitles on screen (or both). Fully customizable by the user previous to runtime, through a client side GUI made in WPF, utilizing 
+the included models, animations and audio clips. The system allows for the easy inclusion of any other of this kind of assets (models,audios,animations).
+
+This repo is for the Client GUI only. For the unity player, will upload upon request (due to the large files like the models included in the player).
